aff67707689a551aec4ef2c18e6607cad70cf29c
tombetthauser <tombetthauser@gmail.com>
2019-10-25 22:29:02 -0700

    Completed all methods / specs but need to tweak for playability

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 require_relative "code"
 
 class Mastermind
@@ -11,6 +13,14 @@ class Mastermind
   def print_matches(code_inst)
     print self.secret_code.num_exact_matches(code_inst)
     print self.secret_code.num_near_matches(code_inst)
+  end
+
+  def ask_user_for_guess
+    print "Enter a code: "
+    input = gets.chomp
+    input_code_inst = Code.from_string(input)
+    self.print_matches(input_code_inst)
+    input_code_inst.pegs == self.secret_code.pegs
   end
 
 end
