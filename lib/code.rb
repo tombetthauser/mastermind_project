ea985d1a1623e7d7dca097b26d191941d621cfdc
tombetthauser <tombetthauser@gmail.com>
2019-10-25 21:01:39 -0700

    Added first five methods for Code class in master_mind project

--- lib/code.rb
+++ lib/code.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Code
   POSSIBLE_PEGS = {
     "R" => :red,
@@ -5,5 +7,26 @@ class Code
     "B" => :blue,
     "Y" => :yellow
   }
+
+  def self.valid_pegs?(chars)
+    chars.all? { |ch| POSSIBLE_PEGS.include?(ch.upcase) } ? true : false
+  end
+
+  def self.random(length)
+    rand_chars_arr = []
+    length.times { rand_chars_arr << POSSIBLE_PEGS.keys.sample }
+    Code.new(rand_chars_arr)
+  end
+
+  def self.from_string(pegs_string)
+    Code.new(pegs_string.split(""))
+  end
+
+  attr_reader :pegs
+
+  def initialize(chars_arr)
+    raise unless Code.valid_pegs?(chars_arr)
+    @pegs = chars_arr.map(&:upcase)
+  end
 
 end
